9e98ec9215eff78e0aa1c825215611d8ffb920d4
Rahul Chavda <rahulchavda6000@gmail.com>
2024-09-02 23:30:03 +0530

    setup server side

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-import express from 'express';
-import dotenv from "dotenv"
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app = express();
-dotenv.config({path: "./config/config.env"})
+dotenv.config({ path: "./config/config.env" });
 
 console.log(process.env.PORT);
 
+app.use(
+  cors({
+    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 export default app;
